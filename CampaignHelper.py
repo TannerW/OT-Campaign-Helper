4a94e17a2e9e5e9c42222d70f54ae845c3ebed53
Tanner <tanner.wilkerson@gmail.com>
2021-01-06 20:54:00 -0500

    count users

--- CampaignHelper.py
+++ CampaignHelper.py
@@ -1,7 +1,4 @@
-import discord
-
 import os
-
 import discord
 from dotenv import load_dotenv
 
@@ -15,19 +12,16 @@ client = discord.Client()
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
+@client.event
+async def on_message(message):
+    id = client.get_guild(ID)
+    channels = ["commands"]
+    valid_users = ["Tanner#1753"]
+
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find("!hello") != -1:
+            await message.channel.send("Hi") 
+        elif message.content == "!users":
+            await message.channel.send(f"""# of Members: {id.member_count}""")
+
 client.run(TOKEN)
-
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print('Logged on as', self.user)
-
-#     async def on_message(self, message):
-#         # don't respond to ourselves
-#         if message.author == self.user:
-#             return
-
-#         if message.content == 'ping':
-#             await message.channel.send('pong')
-
-# client = MyClient()
-# client.run('token')
